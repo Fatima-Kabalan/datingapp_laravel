394365572c8d1c3a72629b9e812bcbaf9a0fb2d0
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-10-04 13:53:09 +0300

    backend:adding chats table in database

---
+++ backend/database/migrations/2022_10_04_105124_chats.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Chats extends Migration
+{
+    public function up()
+    {
+        Schema::create('chats', function (Blueprint $table) {            
+            $table->foreignId('sender_id')->references('id')->on('users');           
+            $table->foreignId('recipient_id')->references('id')->on('users');        
+            $table->string('message');      
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('chats');
+    }
+}
